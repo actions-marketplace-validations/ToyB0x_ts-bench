905fe97e75bd03cc35a7f979b2547b6f0272e83d
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2025-08-16 10:25:42 +0900

    fix: update web deployment workflow and add build check (#260)
    * fix: update web deployment workflow and add build check
    
    * fix: update build check in web deployment workflow
    
    * fix: update web app build command in deployment workflow

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -1,6 +1,19 @@
+import { copyFile } from "node:fs/promises";
+import path from "node:path";
 import type { Config } from "@react-router/dev/config";
 
 export default {
   // SPA mode
   ssr: false,
+
+  // Workaround for Vite's SPA mode with GitHub Pages
+  // ref: https://zenn.dev/ofk/articles/562098fdab1888
+  async buildEnd(args): Promise<void> {
+    if (!args.viteConfig.isProduction) return;
+    const buildPath = args.viteConfig.build.outDir;
+    await copyFile(
+      path.join(buildPath, "index.html"),
+      path.join(buildPath, "404.html"),
+    );
+  },
 } satisfies Config;
